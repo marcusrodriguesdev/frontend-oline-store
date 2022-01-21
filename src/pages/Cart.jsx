0770e61a81e9269e2a6a59243a9b6e65e5c6e883
Tiago Abranges <tiagoabranges@gmail.com>
2022-01-21 16:26:24 -0300

    requisito-3

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        Seu carrinho está vazio
+      </div>
+    );
+  }
+}
+
+export default Cart;
